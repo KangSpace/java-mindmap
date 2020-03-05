3642daf33c9473308e55dd82271f5bf767dec67a
KangSpace <kango2gler@gmail.com>
2020-03-05 14:45:23 +0800

    designpattern: add behavioral 2 patterns

---
+++ src/main/java/org/kangspace/common/designpattern/behavioral/InterpreterPatternTest.java
@@ -0,0 +1,80 @@
+package org.kangspace.common.designpattern.behavioral;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 解释器模式
+ * 给定一种语言，定义它的文法的一种表示，并定义一个解释器，这个解释器使用该表示来解释语言中的句子
+ * 适用性：
+ *  当有一个语言需要解释执行，并且你可以将语言中的句子表示为一个抽象语法树时，可使
+ *  用解释器模式。当存在以下情况是效果最好：
+ *  1. 该文法简单对于复杂的文法，文法的类层次变得庞大而无法管理。
+ *  2. 效率不是一个关键问题，最高效的解释器通常不是通过直接解释语法分析树实现的，而是首先将他们转换为另一种形式
+ * 参与者:
+ *  1. 抽象表达式
+ *     定义一个解释操作
+ *  2. 终结符表达式
+ *  3. 非终结符表达式
+ *  4. Context 上下文对象
+ *     保存除解释器外的其他全局对象
+ *  5. Client
+ *     构建表示该文法定义中的一个特定语句中的抽象语法树，该语法树有终结符表达式和非终结符表达式构建，调用解析操作。
+ * @author kangxuefeng@etiantian.com
+ */
+public class InterpreterPatternTest {
+    interface Expression{
+        void interpreter(Context context);
+    }
+
+    /**
+     * 增强解释器
+     */
+    static class AdvanceExpression implements Expression{
+        @Override
+        public void interpreter(Context context) {
+            System.out.println("这是增强解析器:"+context.getContent());
+        }
+    }
+    static class DefaultExpression implements Expression{
+        @Override
+        public void interpreter(Context context) {
+            System.out.println("这是默认解析器:"+context.getContent());
+        }
+    }
+    static class Context {
+        private String content;
+        private List<Expression> expressions = new ArrayList<>();
+
+        public Context(String content) {
+            this.content = content;
+        }
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void add(Expression expression) {
+            expressions.add(expression);
+        }
+        public List<Expression> getList(){
+            return expressions;
+        }
+    }
+
+
+    static void main(){
+        Context context = new Context("public void main()");
+        context.add(new DefaultExpression());
+        context.add(new AdvanceExpression());
+        context.getList().forEach(t->{
+            t.interpreter(context);
+        });
+    }
+    public static void main(String[] args) {
+        main();
+    }
+}
---
+++ src/main/java/org/kangspace/common/designpattern/behavioral/IteratorPatternTest.java
@@ -0,0 +1,121 @@
+package org.kangspace.common.designpattern.behavioral;
+
+/**
+ * 迭代器模式
+ * 概述: 提供一种方法顺序访问一个聚合对象中的各种元素，而又不暴露该对象的内部表示。
+ * 适用性:
+ *      1. 访问一个聚合对象而无需暴露它的内部表示
+ *      2. 支持聚合对象的多种遍历
+ *      3. 为遍历不用的聚合结构提供一个统一的接口(即，支持多态迭代)
+ * 参与者:
+ *      1. Iterator 迭代器定义访问和遍历元素的接口
+ *      2. ConcreterIterator 具体迭代器实现迭代器接口。
+ *                           对该聚合遍历时跟踪当前位置
+ *      3. Aggregate 聚合定义创建相应聚合对象的接口
+ *      4. ConcreteAggregate 具体聚合实现创建相应迭代器的接口，返回ConcreteIterator
+ * @author kangxuefeng@etiantian.com
+ */
+public class IteratorPatternTest {
+    /**
+     * 可迭代对象接口
+     * 定义创建迭代器操作接口
+     */
+    interface Iterable<T>{
+        Iterator<T> iterator();
+    }
+
+    /**
+     * 迭代器接口
+     * 定义访问和遍历元素接口
+     * @param <T>
+     */
+    interface Iterator<T>  {
+        T first();
+        T last();
+        T next();
+        boolean hasNext();
+    }
+
+    /**
+     * 聚合对象
+     */
+    static class Aggregator<T> implements Iterable<T> {
+        private Object[] objs;
+        private int count;
+        public Aggregator(){
+            this.objs = new Object[100];
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return new AggregatorIterator<>();
+        }
+
+        public void add(T t) {
+            objs[count] = (Object)t;
+            count++;
+        }
+
+        public T get(int i) {
+            if(i>=count || i<0) throw new ArrayIndexOutOfBoundsException();
+            return (T)objs[i];
+        }
+
+        public int size() {
+            return count;
+        }
+
+        /**
+         * 具体的迭代器实现
+         * 实现迭代接口,记录迭代位置
+         */
+        private class AggregatorIterator<T> implements Iterator<T>{
+            private int position = -1;
+            private Object[] data = objs;
+            @Override
+            public T first() {
+                return (T)data[0];
+            }
+
+            @Override
+            public T last() {
+                return (T)data[size()-1];
+            }
+
+            @Override
+            public T next() {
+                return (T)data[position];
+            }
+
+            @Override
+            public boolean hasNext() {
+                return ++position<size();
+            }
+        }
+    }
+
+
+    static void main(){
+        Aggregator<Integer> list = new Aggregator<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        //第一种迭代 iterator
+        System.out.println("===========第一种迭代 iterator===========");
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        //第二种迭代 for
+        System.out.println("===========第二种迭代 for===========");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+    }
+    public static void main(String[] args) {
+        main();
+    }
+}
